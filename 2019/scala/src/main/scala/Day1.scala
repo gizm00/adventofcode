1325147da2e51b448491e2d3a8e44c5d263a9935
gizm00 <jennifer.m.leonard@gmail.com>
2019-12-01 20:57:36 -0800

    adding scala solution for d1 part 1

---
+++ 2019/scala/src/main/scala/Day1.scala
@@ -0,0 +1,31 @@
+import scala.util.{Try, Using}
+import scala.io.Source
+
+object Day1 extends App {
+  def SOURCE_FILE = "/Users/gizmo/dev/adventofcode/2019/input_files/d1_input_module_masses.txt"
+
+  val module_list = readFromFile(SOURCE_FILE)
+  val fuelRequired = module_list
+    .map(_.toInt)
+    .map(calcFuel)
+    .sum
+  println(s"Required fuel: ${fuelRequired}")
+
+  def readFromFile(fileName:String): List[String] = {
+    // Figure out how to wrap in Using
+    return Source.fromFile(fileName).getLines.toList
+  }
+
+  def calcFuel(moduleMass:Int) : Int = {
+    if (moduleMass == 0)
+      return 0
+    val fuelRequired = (moduleMass/3).toInt - 2
+    if (fuelRequired <= 0)
+      return 0
+    else
+      return fuelRequired
+  }
+
+}
+
+
