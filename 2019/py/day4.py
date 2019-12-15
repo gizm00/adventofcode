c6fdca3aea644524b8489a11a94f73c24cdcc6a2
gizm00 <4462255+gizm00@users.noreply.github.com>
2019-12-14 21:45:31 -0800

    woohoo my faith in my abilitys is restored, thanks day4!

--- 2019/py/day4.py
+++ 2019/py/day4.py
@@ -1,7 +1,9 @@
 
+import logging
+
 INPUT_MIN = 123257
 INPUT_MAX = 647015
-
+logging.basicConfig(level = logging.ERROR)
 
 def is_password(input):
     """
@@ -17,8 +19,13 @@ def is_password(input):
     str_input = str(input)
     has_adjacent_digits = False
     digits_same_or_increase_l_r = True
+    digit_count = {}
     for i in range(len(str_input)-1):
         # are there any adjacent digits that are the same?
+        if str_input[i] in digit_count.keys():
+            digit_count[str_input[i]] += 1
+        else:
+            digit_count[str_input[i]] = 1
         if str_input[i] == str_input[i+1]:
             has_adjacent_digits = True
 
@@ -26,7 +33,25 @@ def is_password(input):
         if int(str_input[i]) > int(str_input[i+1]):
             digits_same_or_increase_l_r = False
 
-    return has_adjacent_digits and digits_same_or_increase_l_r
+    logging.debug("Digit counts: {}".format(digit_count))
+
+    # if we havent met the rules return false before bothering
+    # to calculate the greater than 2 repeating rule for part 2
+    if not(has_adjacent_digits and digits_same_or_increase_l_r):
+        return False
+
+    # because loop stops at -2 need to check the last digit
+    # for repeating rule
+    if str_input[-1] in digit_count.keys():
+        digit_count[str_input[-1]] += 1
+
+    # For part 2, we need to check if the adjacent digits
+    # have > 2 counts. If so they are disqualified
+    just_two = [d for d in digit_count.keys() if digit_count[d] == 2]
+    if len(just_two) > 0:
+        return True
+    else:
+        return False
 
 
 if __name__ == '__main__':
--- 2019/py/test_day4.py
+++ 2019/py/test_day4.py
@@ -3,9 +3,12 @@ import pytest
 from day4 import is_password
 
 @pytest.mark.parametrize("input, expected", [
-(111111, True),
+(111111, False), # before part 2 modifications this was true
 (223450, False),
-(123789, False)
+(123789, False),
+(112233, True),
+(123444, False),
+(111122, True)
 ])
 def test_is_password(input, expected):
     assert is_password(input) == expected
